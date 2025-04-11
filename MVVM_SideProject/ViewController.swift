dd825b55dd634a8f859069127ea986722f91c429
Willy <willy548798@gmail.com>
2025-04-11 11:54:07 +0800

    Initial Commit

---
+++ MVVM_SideProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_SideProject
+//
+//  Created by Willy Hsu on 2025/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
