ed96365b701a7c050b78d254cd2094076d427d0a
Willy <willy548798@gmail.com>
2025-04-16 12:31:49 +0800

    Setup Tabbar controller

---
+++ MVVM_SideProject/Controller/MainTabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarController.swift
+//  MVVM_SideProject
+//
+//  Created by Willy Hsu on 2025/4/16.
+//
+
+import Foundation
---
+++ MVVM_SideProject/View/TipCalculator/TipCalculatorView.swift
@@ -0,0 +1,8 @@
+//
+//  TipCalculatorView.swift
+//  MVVM_SideProject
+//
+//  Created by Willy Hsu on 2025/4/16.
+//
+
+import Foundation
